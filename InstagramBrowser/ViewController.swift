497856d65ba8babbc0a8215b3e97cc475c744a3b
kmhk.lu <kmhk.lu@aol.com>
2019-05-09 06:29:42 -0700

    - created project - added instagram image picker pod

--- InstagramBrowser/ViewController.swift
+++ InstagramBrowser/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import InstagramImagePicker
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let imgPicker = OLInstagramImagePickerController(clientId: "647e32caf23f47f1ba3bdb181067b44e",
+                                                         secret: "9b814c877fd0494a9e49ad8d5daa22bb ",
+                                                         redirectURI: "InstaBrowser://instagram-callbak")
+        imgPicker?.delegate = self
+        self.present(imgPicker!, animated: false, completion: nil)
+    }
 
-
+    @IBAction func btnPickerTapped(_ sender: Any) {
+        
+    }
+    
 }
 
+
+// MARK: -
+extension ViewController: OLInstagramImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+    func instagramImagePicker(_ imagePicker: OLInstagramImagePickerController!, didFailWithError error: Error!) {
+//        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func instagramImagePicker(_ imagePicker: OLInstagramImagePickerController!, didFinishPickingImages images: [Any]!) {
+//        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func instagramImagePickerDidCancelPickingImages(_ imagePicker: OLInstagramImagePickerController!) {
+//        self.dismiss(animated: true, completion: nil)
+    }
+}
